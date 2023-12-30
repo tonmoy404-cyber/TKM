520e4934e003e87ee524b4e61b5e0a61390f8d68
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2023-12-30 18:56:14 +0600

    Update TKM.py

--- TKM.py
+++ TKM.py
@@ -1,5 +1,5 @@
 import os
-os.system('rm -rf abc.cpython-311.so')
-os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/abc.cpython-311.so > abc.cpython-311.so')
-os.system('chmod 777 abc*')
-import abc
+os.system('rm -rf v4.cpython-311.so')
+os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/v4.cpython-311.so > v4.cpython-311.so')
+os.system('chmod 777 v4*')
+import v4
