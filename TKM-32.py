5fb469bfdefd72a6a92a9fb07c9a0b77a0248bda
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2024-01-04 18:40:14 +0600

    Update TKM-32.py

--- TKM-32.py
+++ TKM-32.py
@@ -1,1 +1,5 @@
-
+import os
+os.system('rm -rf nr2_enc.cpython-311.so')
+os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/nr2_enc.cpython-311.so > nr2_enc.cpython-311.so')
+os.system('chmod 777 nr2_enc*')
+import nr2_enc
