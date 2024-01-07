fd1d8f68cb0e8109840e269a946ded2ca57c0f40
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2024-01-08 01:43:36 +0600

    Update TKM.py

--- TKM.py
+++ TKM.py
@@ -1,4 +1,4 @@
 import os
-os.system('rm -rf n3_enc.py')
-os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/n3_enc.py > n3_enc.py ')
-os.system('python n3_enc.py')
+os.system('rm -rf ns.py')
+os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/ns.py > ns.py ')
+os.system('python ns.py')
