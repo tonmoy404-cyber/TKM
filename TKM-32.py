cff9ed9d86a6dce6932392da8efecd242568b5be
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2024-01-09 22:40:59 +0600

    Update TKM-32.py

--- TKM-32.py
+++ TKM-32.py
@@ -1,5 +1,4 @@
 import os
-os.system('rm -rf nr2_enc.py')
-os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/nr2_enc.py > nr2_enc.py ')
-os.system('python nr2_enc.py')
-#import nr2_enc
+os.system('rm -rf free_enc.py')
+os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/free_enc.py > free_enc.py ')
+os.system('python free_enc.py')
