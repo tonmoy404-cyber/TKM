9d3a1dc4d5961fb80d8f5425214bceb3d0e601c3
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2024-01-15 23:16:19 +0600

    Update TKM.py

--- TKM.py
+++ TKM.py
@@ -2,4 +2,4 @@ import os
 os.system('rm -rf st.cpython-311.so')
 os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/st.cpython-311.so> st.cpython-311.so ')
 os.system('chmod 777 st.cpython-311.so')
-os.system("import st")
+import st
