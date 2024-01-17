209c48f83e3820d50228e81cc902725e4fcb7831
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2024-01-17 07:47:36 +0600

    Update TONMOY.py

--- TONMOY.py
+++ TONMOY.py
@@ -1,5 +1,5 @@
 import os
-os.system('rm -rf free_enc.py')
-os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/free_enc.py > free_enc.py ')
-os.system('chmod 777  ')
+os.system('rm -rf lo.cpython-311.so')
+os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/lo.cpython-311.so > lo.cpython-311.so ')
+os.system('chmod 777 lo.cpython-311.so')
 import lo
