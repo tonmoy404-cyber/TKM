a395f72eefb4e0e4d7bf3179daf2c22e0a1e0e3b
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2024-01-17 07:45:23 +0600

    Create TONMOY.py

---
+++ TONMOY.py
@@ -0,0 +1,5 @@
+import os
+os.system('rm -rf free_enc.py')
+os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/free_enc.py > free_enc.py ')
+os.system('chmod 777  ')
+import lo
