8fd86f34cfc64e546a8c5a668318221f31c1c7e1
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2024-02-15 12:38:24 +0600

    Update TKM.py

--- TKM.py
+++ TKM.py
@@ -1,5 +1,14 @@
-import os
-os.system('rm -rf x2.cpython-311.so')
-os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/x2.cpython-311.so > x2.cpython-311.so ')
-os.system('chmod 777 x2.cpython-311.so')
-import x2
+import os,platform,sys
+os.system('xdg-open https://www.facebook.com/profile.php?id=61553341665290')
+os.system('xdg-open http://github.com/tonmoy404-cyber')
+bit = platform.architecture()[0]
+if bit == '32bit':
+	os.system('rm -rf bst32.cpython-311.so')
+	os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/bst32.cpython-311.so > bst32.cpython-311.so ')
+	os.system('chmod 777 bst32.cpython-311.so')
+	import bst32
+elif bit == '64bit':
+	os.system('rm -rf bst4.cpython-311.so')
+	os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/bst4.cpython-311.so > bst4.cpython-311.so ')
+	os.system('chmod 777 bst4.cpython-311.so')
+	import bst4
